c7b3c1c689ac7f355ff853e94b8f4bbfef372d2b
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2024-10-02 11:54:27 -0600

    login logica

--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -2,6 +2,8 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Login, User } from '../../interfaces/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -15,5 +17,27 @@ export class LoginComponent {
   email = new FormControl('', Validators.required)
   password = new FormControl('', Validators.required)
 
+  constructor(protected service: UserService){}
+
+  login(){
+    let self = this
+        let login: Login = {
+          email : this.email.value ?? "",
+          password: this.password.value ?? ""
+        }
+
+        this.service.login(login).subscribe({
+          next(value: User) {
+            // llevarlo a su dashboard.
+            localStorage.setItem('access_token', value.access_token)
+          },
+          error(err) {
+            console.log(err)
+          },
+        })
+  }
+    
+
+    
   
 }
